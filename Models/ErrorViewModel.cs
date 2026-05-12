0cb80f9e1a575f1ba45c485a1dc3ed9899643a08
CATALINA SOL MILSZTAIN <50600085@ort>
2026-05-12 08:47:52 -0300

    Creacion del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_02___Recetiify.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
