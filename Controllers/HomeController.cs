7a5f686ba66cbdbcc67484c6298abdb042a07d8e
CATALINA SOL MILSZTAIN <50600085@ort>
2026-05-13 09:05:05 -0300

    Casi terminado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,4 +28,14 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult GenerarReceta(Receta receta)
+    {
+        ViewBag.nombre = receta.Nombre;
+        ViewBag.edad = receta.CalcularEdad();
+        ViewBag.plato = receta.determinarPlato();
+        ViewBag.tiempo = receta.calcularTiempo();
+        ViewBag.dificultad = receta.determinarDificultad();
+        return View("Resultado");
+    }
 }
--- Models/Receta.cs
+++ Models/Receta.cs
@@ -1,5 +1,5 @@
 public class Receta{
-    public string NombreCocinero {get;set;}
+    public string Nombre {get;set;}
     public DateTime FechaNacimiento {get;set;}
     public string TipoComida {get;set;}
     public double PresupuestoDisp {get;set;}
@@ -7,7 +7,7 @@ public class Receta{
 
     public Receta (string nombre, DateTime fecha, string tipo, double presu, int cant)
     {
-        this.NombreCocinero = nombre;
+        this.Nombre = nombre;
         this.FechaNacimiento = fecha;
         this.TipoComida = tipo;
         this.PresupuestoDisp = presu;
@@ -22,6 +22,91 @@ public class Receta{
             edad -= 1;
         }
         return edad;
+    }
+
+    public string determinarPlato(){
+        string platoSugerido;
+        if(TipoComida == "Caliente"){
+            if(PresupuestoDisp < 3000){
+                platoSugerido = "Fideos Con Manteca";
+            }
+            else if(3000 <= PresupuestoDisp <= 7000){
+                platoSugerido = "Arroz con verduras salteadas";
+            }
+            else{
+                platoSugerido = "Pollo al horno con guarnición";
+            }
+             }
+        else if(TipoComida == "Fría"){
+            if(PresupuestoDisp < 3000){
+                platoSugerido = "Ensalada simple";
+            }
+            else if(3000 <= PresupuestoDisp <= 7000){
+                platoSugerido = "Ensalada completa con proteína";
+            }
+            else{
+                platoSugerido = "Tabla de fiambres y quesos";
+            }
+        }
+        return platoSugerido;
+       
+    }
+
+    public int calcularTiempo(){
+        int minutosEst;
+        if(TipoComida == "Caliente"){
+            if (CantPersonas == 1 || CantPersonas == 2 || CantPersonas == 3){
+                minutosEst = 20;
+            }
+            else if(4 <= CantPersonas <= 7){
+                minutosEst = 40;
+            }
+            else{
+                minutosEst = 80;
+            }
+             }
+        else if(TipoComida == "Fría"){
+             if (CantPersonas == 1 || CantPersonas == 2 || CantPersonas == 3){
+                minutosEst = 10;
+            }
+            else if(4 <= CantPersonas <= 7){
+                minutosEst = 20;
+            }
+            else{
+                minutosEst = 40;
+            }
+        }
+        return minutosEst;
+    }
+
+    public string determinarDificultad(){
+        string dificultad;
+
+        if(PresupuestoDisp < 3000){
+            if (CantPersonas == 1 || CantPersonas == 2 || CantPersonas == 3){
+                dificultad = "Principiante";
+            }
+            else if(4 <= CantPersonas <= 7){
+                dificultad = "Intermedio";
+            }
+        }
+        else if(3000 <= PresupuestoDisp <= 7000){
+             if (CantPersonas == 1 || CantPersonas == 2 || CantPersonas == 3){
+                dificultad = "Intermedio";
+            }
+            else if(4 <= CantPersonas){
+               dificultad = "Intermedio";
+            }
+        }
+        else{
+            if(1 <= CantPersonas <= 7){
+                dificultad = "Intermedio";
+            }
+            else{
+                dificultad = "Avanzado";
+            }
+        }
+        return dificultad;
     }
 
 
